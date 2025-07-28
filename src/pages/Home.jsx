ab84b4332d140e8b3efafbdce1b5a223fea7ab47
Leandro <leacalca@gmail.com>
2025-07-28 15:54:19 -0300

    estilos en home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,23 @@
 import { Link } from "react-router-dom";
+import UserImg from '../img/pesas.webp'
+import AdmImg from '../img/adm.jpg'
 
 const Home = () => {
   return (
     <main>
-      <section className="container">
-        <div>
+      <section className="container home">
+        <div className="cards-container">
           <Link to={"/user"}>
-            <button>Usuario</button>
+            <article className="card">
+              <img src={UserImg} alt="imagen de usuario" className="card-img" />
+              <h2 className="card-title">Usuario</h2>
+            </article>
           </Link>
           <Link to={"/admin"}>
-            <button>Administracion</button>
+            <article className="card">
+              <img src={AdmImg} alt="imagen de administracion" className="card-img" />
+              <h2 className="card-title">Administracion</h2>
+            </article>
           </Link>
         </div>
       </section>
