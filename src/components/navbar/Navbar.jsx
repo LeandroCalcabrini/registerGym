e728445f7f1aac7693335c5347c221901cbe824d
Leandro <leacalca@gmail.com>
2025-07-29 14:40:46 -0300

    header mobile con burgermenu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,27 @@
 import { NavLink } from "react-router-dom";
 import "../navbar/navbar.css";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { IoClose } from "react-icons/io5";
+
+import { useState } from "react";
 
 const Navbar = () => {
+  const [burger, setBurger] = useState(false);
   return (
     <header className="header">
       <strong className="logo">Star Gym</strong>
-      <nav className="navbar">
+      <nav className={`navbar ${burger ? "active" : ""}`}>
+        <IoClose
+          onClick={() => setBurger(false)}
+          size={25}
+          className="closeIcon"
+        />
         <NavLink
           className={({ isActive }) =>
             isActive ? "nav-link active" : "nav-link"
           }
           to={"/"}
+          onClick={() => setBurger(false)}
         >
           Inicio
         </NavLink>
@@ -20,6 +30,7 @@ const Navbar = () => {
             isActive ? "nav-link active" : "nav-link"
           }
           to={"/user"}
+          onClick={() => setBurger(false)}
         >
           Usuario
         </NavLink>
@@ -28,11 +39,18 @@ const Navbar = () => {
             isActive ? "nav-link active" : "nav-link"
           }
           to={"/admin"}
+          onClick={() => setBurger(false)}
         >
           Administracion
         </NavLink>
       </nav>
-      <GiHamburgerMenu size={30} className="burgerMenu"/>
+        {!burger && (
+          <GiHamburgerMenu
+            onClick={() => setBurger(true)}
+            size={30}
+            className="burgerMenu"
+          />
+        )}
     </header>
   );
 };
